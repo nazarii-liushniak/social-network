b0e3183319d041acbe3e49a6bc4cdcaccf71a746
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2025-09-13 22:39:19 +0300

    Added repositories extension

--- SocialNetwork.WebAPI/Program.cs
+++ SocialNetwork.WebAPI/Program.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using SocialNetwork.WebAPI.Data;
-using SocialNetwork.WebAPI.Interfaces.Repositories;
-using SocialNetwork.WebAPI.Repositories;
+using SocialNetwork.WebAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +12,7 @@ builder.Services.AddDbContext<SocialNetworkDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SocialNetworkDb"))
 );
 
-builder.Services.AddScoped<ICommentRepository, CommentRepository>();
-builder.Services.AddScoped<IFollowRepository, FollowRepository>();
-builder.Services.AddScoped<ILikeRepository, LikeRepository>();
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-builder.Services.AddScoped<IPostRepository, PostRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddRepositories();
 
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
