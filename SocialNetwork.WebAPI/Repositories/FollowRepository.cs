866ccedd37b59731faf081d44030e77cc4cb2afb
Nazarii Liushniak <114431377+nazarii-liushniak@users.noreply.github.com>
2025-09-14 23:28:43 +0300

    Feature/add getting follows by user (#8)
    * Added getting followings by user
    
    * Added some fixes

--- SocialNetwork.WebAPI/Repositories/FollowRepository.cs
+++ SocialNetwork.WebAPI/Repositories/FollowRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using SocialNetwork.WebAPI.Data;
 using SocialNetwork.WebAPI.Entities;
 using SocialNetwork.WebAPI.Interfaces.Repositories;
@@ -17,17 +18,27 @@ public class FollowRepository(SocialNetworkDbContext context) : IFollowRepositor
         return follow;
     }
 
+    public async Task<IEnumerable<Follow>> GetFollowingsByUser(Guid userId)
+    {
+        var users = await _context.Follows
+            .Where(f => f.FollowerId == userId)
+            .Include(f => f.Followee)
+            .ToListAsync();
+
+        return users;
+    }
+
     public async Task<Follow?> DeleteFollowAsync(Guid followId)
     {
-        var follow  = await _context.Follows.FindAsync(followId);
-        
+        var follow = await _context.Follows.FindAsync(followId);
+
         if (follow == null)
             return null;
-        
+
         _context.Follows.Remove(follow);
-        
+
         await _context.SaveChangesAsync();
-        
+
         return follow;
     }
 }
