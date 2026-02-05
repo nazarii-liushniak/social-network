9307e0ed562923b8d8d193ddd6549c5e44024b0b
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2026-02-05 14:41:26 +0200

    Fixed typo in JwtAuthenticationExtensions.cs

--- SocialNetwork.WebAPI/Program.cs
+++ SocialNetwork.WebAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext(builder.Configuration);
 builder.Services.AddRepositories();
-builder.Services.AddJwtAuthenticaion(builder.Configuration);
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
