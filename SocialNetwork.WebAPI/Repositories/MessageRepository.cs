7a5c14227c93546dedade871d13240a3a097e4ec
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2026-07-21 20:03:01 +0300

    Changed MessageRepository

--- SocialNetwork.WebAPI/Repositories/MessageRepository.cs
+++ SocialNetwork.WebAPI/Repositories/MessageRepository.cs
@@ -2,102 +2,122 @@ using Microsoft.EntityFrameworkCore;
 using SocialNetwork.WebAPI.Data;
 using SocialNetwork.WebAPI.Entities;
 using SocialNetwork.WebAPI.Interfaces.Repositories;
+using SocialNetwork.WebAPI.Models.Message;
+using SocialNetwork.WebAPI.Models.User;
 
 namespace SocialNetwork.WebAPI.Repositories;
 
 public class MessageRepository(SocialNetworkDbContext context) : IMessageRepository
 {
-    private readonly SocialNetworkDbContext _context = context;
-
-    public async Task AddMessageAsync(Message message)
+    public void AddMessage(Message message)
     {
-        await _context.Messages.AddAsync(message);
-        
-        await _context.SaveChangesAsync();
+        context.Messages.Add(message);
     }
 
-    public async Task<IEnumerable<Message>> GetMessagesAsync(
-        Guid userId,
+    public async Task<IReadOnlyList<MessageResponse>> GetMessagesAsync(Guid userId,
         Guid otherUserId,
-        DateTime timestamp,
-        Guid messageId,
-        int limit)
+        DateTimeOffset? timestamp,
+        Guid? messageId,
+        int limit,
+        CancellationToken cancellationToken = default)
     {
-        var messages = await _context.Messages
+        limit = Math.Clamp(limit, 0, 100);
+
+        var query = context.Messages
             .Where(m => (m.SenderId == userId && m.ReceiverId == otherUserId) 
                     || (m.SenderId == otherUserId && m.ReceiverId == userId))
             .OrderByDescending(m => m.SentAt)
             .ThenBy(m => m.Id)
-            .Where(m => m.SentAt < timestamp ||
-                        (m.SentAt == timestamp && m.ReceiverId > otherUserId))
+            .AsQueryable();
+
+        if (timestamp.HasValue && messageId.HasValue)
+        {
+            query = query.Where(m => m.SentAt < timestamp.Value
+                || (m.SentAt == timestamp.Value && m.Id > messageId.Value));
+        }
+            
+        return await query
             .Take(limit)
-            .ToListAsync();
-        
-        return messages;
+            .Select(m => new MessageResponse()
+            {
+                Id = m.Id,
+                Direction = m.SenderId == userId
+                    ? MessageDirection.Outgoing
+                    : MessageDirection.Incoming,
+                Content = m.Content,
+                SentAt = m.SentAt,
+            })
+            .ToListAsync(cancellationToken);
     }
 
-    public async Task<IEnumerable<(
-            User OtherUser,
-            string LastMessageContent,
-            DateTime LastMessageTimestamp
-    )>> GetChatsAsync(Guid userId)
+    public async Task<IReadOnlyList<ChatResponse>> GetChatsAsync(Guid userId,
+        DateTimeOffset? timestamp,
+        Guid? otherUserId,
+        int limit,
+        CancellationToken cancellationToken = default)
     {
-        var chats = await _context.Messages
+        limit = Math.Clamp(limit, 0, 100);
+
+        var query = context.Messages
             .Where(m => m.SenderId == userId || m.ReceiverId == userId)
             .Select(m => new
             {
                 Message = m,
-                OtherUserId = m.SenderId == userId ? m.ReceiverId : m.SenderId
+                OtherUserId = m.SenderId == userId ? m.ReceiverId : m.SenderId,
             })
-            .GroupBy(x => x.OtherUserId)
+            .GroupBy(a => a.OtherUserId)
             .Select(g => g
-                .OrderByDescending(x => x.Message.SentAt)
-                .Select(x => new
-                {
-                    OtherUser = x.Message.SenderId == userId
-                        ? x.Message.Receiver
-                        : x.Message.Sender,
-                    LastMessageContent = x.Message.Content,
-                    LastMessageSentAt = x.Message.SentAt,
-                })
-                .FirstOrDefault()
-            )
-            .OrderByDescending(c => c!.LastMessageSentAt)
-            .ToListAsync();
+                .OrderByDescending(a => a.Message.SentAt)
+                .Select(a => new ChatResponse()
+                    {
+                        OtherUser = new Author()
+                        {
+                            Id = a.Message.SenderId == userId
+                                ? a.Message.Receiver.Id
+                                : a.Message.Sender.Id,
+                            Username = a.Message.SenderId == userId
+                                ? a.Message.Receiver.Username
+                                : a.Message.Sender.Username,
+                            FullName = a.Message.SenderId == userId
+                                ? a.Message.Receiver.FullName
+                                : a.Message.Sender.FullName,
+                            ProfileImageUrl = a.Message.SenderId == userId
+                                ? a.Message.Receiver.ProfileImageUrl
+                                : a.Message.Sender.ProfileImageUrl,
+                        },
+                        LastMessageContent = a.Message.Content,
+                        LastMessageTimestamp = a.Message.SentAt,
+                    })
+                .First()
+            );
+        
+        if (timestamp.HasValue && otherUserId.HasValue)
+        {
+            query = query.Where(c => c.LastMessageTimestamp < timestamp.Value 
+                || (c.LastMessageTimestamp == timestamp.Value && c.OtherUser.Id > otherUserId.Value));
+        }
 
-        return chats.Select(c => (c.OtherUser, c.LastMessageContent, c.LastMessageSentAt));
+        return await query
+            .OrderByDescending(c => c.LastMessageTimestamp)
+            .ThenBy(c => c.OtherUser.Id)
+            .Take(limit)
+            .ToListAsync(cancellationToken);
     }
 
-    public async Task<Message?> UpdateMessageAsync(Message message)
+    public void DeleteMessage(Message message)
     {
-        var dbMessage = await _context.Messages.FindAsync(message.Id);
-        
-        if (dbMessage == null)
-            return null;
-        
-        message.Id = dbMessage.Id;
-        message.SenderId = dbMessage.SenderId;
-        message.ReceiverId = dbMessage.ReceiverId;
-        message.SentAt = dbMessage.SentAt;
-        
-        _context.Messages.Update(message);
-        
-        await _context.SaveChangesAsync();
-        
-        return message;
+        context.Messages.Remove(message);
     }
 
-    public async Task<Message?> DeleteMessageAsync(Guid messageId)
+    public async Task DeleteUserMessagesAsync(Guid userId, CancellationToken cancellationToken = default)
     {
-        var message = await _context.Messages.FindAsync(messageId);
-        
-        if (message == null)
-            return null;
-        
-        _context.Messages.Remove(message);
-        
-        await _context.SaveChangesAsync();
-        
-        return message;
+        await context.Messages
+            .Where(m => m.SenderId == userId)
+            .ExecuteDeleteAsync(cancellationToken);
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await context.SaveChangesAsync(cancellationToken);
     }
 }
