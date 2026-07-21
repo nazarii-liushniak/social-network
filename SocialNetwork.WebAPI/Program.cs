5067f8ef632900a3d2b3b9fa7dc9e085ae78a217
Nazarii Liushniak <nazarii.liushniak.pz.2022@lpnu.ua>
2026-07-21 20:38:10 +0300

    Changed Program.cs

--- SocialNetwork.WebAPI/Program.cs
+++ SocialNetwork.WebAPI/Program.cs
@@ -3,26 +3,20 @@ using SocialNetwork.WebAPI.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext(builder.Configuration);
 builder.Services.AddRepositories();
 builder.Services.AddServices();
+builder.Services.AddContexts();
 builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddSignalR();
+builder.Services.AddMemoryCache();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton(TimeProvider.System);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
